804fa4f73d4fe9d49d8003eeb747ae9addd72287
Builder <deploy@example.com>
2026-03-02 17:33:38 +1100

    build: Configure Vite to alias `react-dom/server` to `react-dom/server.edge` and update Wrangler compatibility date.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,11 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        'react-dom/server': 'react-dom/server.edge'
+      }
+    },
     build: {
       rollupOptions: {
         external: ['sharp']
